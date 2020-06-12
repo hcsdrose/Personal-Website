606f3072972eb1de25c945185c108c1ae6be073c
Chris Rose <chrisrose@chriss-mbp.lan>
2020-06-12 13:16:46 -0400

    update sw

--- js/sw.js
+++ js/sw.js
@@ -1,32 +1,20 @@
-// we'll version our cache (and learn how to delete caches in
-// some other post)
 const cacheName = 'v1::static';
 
 self.addEventListener('install', e => {
-  // once the SW is installed, go ahead and fetch the resources
-  // to make this work offline
   e.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll([
         '/',
         '/index.html',
         '/css/style.css',
-        /*
-          DEAR READER,
-          ADD A LIST OF YOUR ASSETS THAT
-          YOU WANT TO WORK WHEN OFFLINE
-          TO THIS ARRAY OF URLS
-        */
+        '/js/jquery-3.5.1.min.js'
       ]).then(() => self.skipWaiting());
     })
   );
 });
 
-// when the browser fetches a url, either response with
-// the cached object or go ahead and fetch the actual url
 self.addEventListener('fetch', event => {
   event.respondWith(
-    // ensure we check the *right* cache to match against
     caches.open(cacheName).then(cache => {
       return cache.match(event.request).then(res => {
         return res || fetch(event.request)
