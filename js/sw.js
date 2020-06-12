e0ea1afcb91e3195175ae2517ea587ead88ad3d8
Chris Rose <chrisrose@chriss-mbp.lan>
2020-06-12 13:25:38 -0400

    offline update

--- js/sw.js
+++ js/sw.js
@@ -25,4 +25,10 @@ self.addEventListener('fetch', event => {
 
 self.addEventListener('fetch', function(event) {
  console.log(event.request.url);
+
+ event.respondWith(
+   caches.match(event.request).then(function(response) {
+     return response || fetch(event.request);
+   })
+ );
 });
