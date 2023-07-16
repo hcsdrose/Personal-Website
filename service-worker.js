354f97d8693474f1c14f79b0ce445138ac1cd2ae
Chris Rose <crose@abre.io>
2023-07-16 16:33:25 -0400

    update

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const cacheName = 'christopher-rose-v1';
+const cacheFiles = [
+  '/',
+  '/index.html',
+  '/css/style-1.0.0.css',
+  '/javascript/app-1.0.0.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(cacheFiles);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== cacheName) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
